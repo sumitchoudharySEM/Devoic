7ded7a95cacf2133c0714095f3dba00597825cd8
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-03-27 23:32:38 +0530

    dekstop navbar fixed

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 //create routes view for home about and other pages
 Route::view('/','pages/home');
 Route::view('/about','pages/about');
-Route::view('/cource','pages/cource');
+Route::view('/cources','pages/cources');
 Route::view('/programs','pages/programs');
